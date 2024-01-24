26371aa87e4026e00e2de4de55441c3f28d414a8
zyy <zyy2008@vip.qq.com>
2024-01-24 09:34:21 +0800

    提交

--- src/components/jeecg/thirdApp/JThirdAppDropdown.vue
+++ src/components/jeecg/thirdApp/JThirdAppDropdown.vue
@@ -8,36 +8,46 @@
       </a-menu>
     </template>
   </a-dropdown>
-  <a-button v-else-if="syncToApp" type="primary" preIcon="ant-design:sync-outlined" @click="handleMenuClick({ key: 'to-app' })"
+  <a-button
+    v-else-if="syncToApp"
+    type="primary"
+    preIcon="ant-design:sync-outlined"
+    @click="handleMenuClick({ key: 'to-app' })"
     >同步{{ name }}</a-button
   >
-  <a-button v-else type="primary" preIcon="ant-design:sync-outlined" @click="handleMenuClick({ key: 'to-local' })">同步{{ name }}到本地</a-button>
+  <a-button
+    v-else
+    type="primary"
+    preIcon="ant-design:sync-outlined"
+    @click="handleMenuClick({ key: 'to-local' })"
+    >同步{{ name }}到本地</a-button
+  >
 </template>
 
 <script lang="ts" setup>
-  /* JThirdAppButton 的子组件，不可单独使用 */
-  import { computed } from 'vue';
+/* JThirdAppButton 的子组件，不可单独使用 */
+import { computed } from "vue";
 
-  const props = defineProps({
-    type: String,
-    name: String,
-    syncToApp: Boolean,
-    syncToLocal: Boolean,
-  });
-  // 声明Emits
-  const emit = defineEmits(['to-app', 'to-local']);
+const props = defineProps({
+  type: String,
+  name: String,
+  syncToApp: Boolean,
+  syncToLocal: Boolean,
+});
+// 声明Emits
+const emit = defineEmits(["to-app", "to-local"]);
 
-  const getSyncToLocal = computed(() => {
-    // 由于企业微信接口变更，将不再支持同步到本地
-    if (props.type === 'wechatEnterprise') {
-      return false;
-    }
-    return props.syncToLocal;
-  });
-
-  function handleMenuClick(event) {
-    emit(event.key, { type: props.type });
+const getSyncToLocal = computed(() => {
+  // 由于企业微信接口变更，将不再支持同步到本地
+  if (props.type === "wechatEnterprise") {
+    return false;
   }
+  return props.syncToLocal;
+});
+
+function handleMenuClick(event) {
+  emit(event.key, { type: props.type });
+}
 </script>
 
 <style scoped></style>
